65d3bff83ef3d558bdb173c42ecdbe516d6ae814
김기은 <w.snowhack@gmail.com>
2020-05-14 11:37:04 +0900

    feat: problem CRUD

--- back/problems/models.py
+++ back/problems/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MultipleChoice(models.Model):
+    category = models.CharField(max_length=10)
+    question = models.TextField()
+    
