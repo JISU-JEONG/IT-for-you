b2bed181278e79302e93591b15221adb0beb4ce7
EHwooKim <ehwoo0707@naver.com>
2020-05-14 16:53:07 +0900

    문제 추가하기 만드는중..

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <Nav msg="Welcome to Your Vue.js App" />
+    <create-question />
   </div>
 </template>
 
 <script>
 import Nav from "@/components/Nav.vue";
+import CreateQuestion from "@/components/CreateQuestion.vue";
 
 export default {
   name: "Home",
   components: {
-    Nav
+    Nav,
+    CreateQuestion
   }
 };
 </script>
