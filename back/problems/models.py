e569669d5ce3f1eede1885b702016fe56897384c
김기은 <w.snowhack@gmail.com>
2020-05-18 15:49:13 +0900

    mod: Problem CRUD complete

--- back/problems/models.py
+++ back/problems/models.py
@@ -7,14 +7,14 @@ class ProblemBasic(models.Model):
     p_question = models.TextField()
     p_type = models.IntegerField()
     p_diff = models.IntegerField()
-    
+    p_code = models.TextField(null=True)
     class Meta:
         managed = False
         db_table = 'problems'
 
 class AnswerBasic(models.Model):
     a_id = models.AutoField(primary_key=True)
-    p_id = models.ForeignKey(ProblemBasic, models.DO_NOTHING, db_column='p_id')
+    p_id = models.ForeignKey(ProblemBasic, db_column='p_id', on_delete=models.CASCADE)
     a_value = models.TextField()
     a_correct = models.BooleanField()
 
--- back/problems/views.py
+++ back/problems/views.py
@@ -29,27 +29,60 @@ def get_probs_detail(request):
 # Create
 @api_view(['POST'])
 def create_prob(request):
-  prob_info = request.data.get('problems')
-  
-  
   # 문제 생성부
+  prob_info = request.data.get('problems')
   p_category = prob_info.get('p_category')
   p_question = prob_info.get('p_question')
   p_type = prob_info.get('p_type')
   p_diff = prob_info.get('p_diff')
   prob = ProblemBasic(p_category=p_category, p_question=p_question, p_type=p_type, p_diff=p_diff)
+  # embed()
   prob.save()
-
   # 보기 생성부
-  # prob_id = prob.p_id
-  # test
+  answers = request.data.get('answers')
+  correct_ans = request.data.get('correct_ans')
+  for ans in answers:
+    # embed()
+    a_value = ans
+    a_correct = False
+    if a_value == correct_ans:
+      a_correct = True
+    # 외래키인 p_id는 숫자가 아닌 문제 객체 그 자체를 받아야 한다.
+    answer = AnswerBasic(p_id=prob, a_value=a_value, a_correct=a_correct)
+    answer.save()
+
   return JsonResponse({'message': 'Success'})
   
-  
-def create_ans(request, prob_pk):
-  ans_list = request.data.get('answers')
-  correct_ans = request.data.get('answers')
-  for ans_idx in range(len(ans_list)):
-    p_id = prob_pk
-    a_value = ans_list
+# Update
+@api_view(['PUT'])
+def update_prob(request, prob_id):
+  # Prob Update
+  prob_info = request.data.get('problems')
+  prob = get_object_or_404(ProblemBasic, p_id=prob_id)
+  prob.p_category = prob_info.get('p_category')
+  prob.p_question = prob_info.get('p_question')
+  prob.p_type = prob_info.get('p_type')
+  prob.p_diff = prob_info.get('p_diff')
+  prob.save()
 
+  # Answer Update
+  new_answers = request.data.get('answers')
+  new_correct_ans = request.data.get('correct_ans')
+  pre_answers = prob.answerbasic_set.all()
+  for ans_idx in range(len(new_answers)):
+    ans = get_object_or_404(AnswerBasic, a_id=pre_answers[ans_idx].a_id)
+    ans.a_value = new_answers[ans_idx]
+    ans.a_correct = False
+    if ans.a_value == new_correct_ans:
+      ans.a_correct = True
+    ans.save()
+
+  return JsonResponse({'message': 'Success'})
+
+
+# Delete
+@api_view(['DELETE'])
+def prob_delete(request, prob_id):
+  prob = ProblemBasic.objects.get(p_id=prob_id)
+  prob.delete()
+  return JsonResponse({'message': 'Success'})
