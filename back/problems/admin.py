73789a52e90f17c07e32525ebacec5152f0cec3e
김기은 <w.snowhack@gmail.com>
2020-05-19 09:44:52 +0900

    mod: Modify Modeling according to DB Table Change

--- back/problems/admin.py
+++ back/problems/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-class ProblemBasicAdmin(admin.ModelAdmin):
-    list_display = ('p_id', 'p_category', 'p_question', 'p_type', 'p_diff', 'p_code')
-    # ordering = ('p_id',)
-admin.site.register(ProblemBasic, ProblemBasicAdmin)
+class ProbCateAdmin(admin.ModelAdmin):
+    list_display = ('pc_id', 'pc_value',)
+    ordering = ('pc_id',)
+admin.site.register(ProbCate, ProbCateAdmin)
 
-class AnswerBasicAdmin(admin.ModelAdmin):
-    list_display = ('a_id', 'p_id', 'a_value', 'a_correct')
+class ProbTypeAdmin(admin.ModelAdmin):
+    list_display = ('pt_id', 'pt_value',)
+admin.site.register(ProbType, ProbTypeAdmin)
+
+class ProbDiffAdmin(admin.ModelAdmin):
+    list_display = ('pd_id', 'pd_value',)
+admin.site.register(ProbDiff, ProbDiffAdmin)
+
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('p_id', 'p_question', 'pc_id', 'pt_id', 'pd_id',)
     # ordering = ('p_id',)
-admin.site.register(AnswerBasic, AnswerBasicAdmin)
+admin.site.register(Problem, ProblemAdmin)
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('a_id', 'p_id', 'a_value', 'a_correct',)
+    # ordering = ('p_id',)
+admin.site.register(Answer, AnswerAdmin)
--- back/problems/models.py
+++ back/problems/models.py
@@ -1,20 +1,44 @@
 from django.db import models
 
 # Create your models here.
-class ProblemBasic(models.Model):
+class ProbCate(models.Model):
+    pc_id = models.AutoField(primary_key=True)
+    pc_value = models.CharField(max_length=200)
+    class Meta:
+        managed = False
+        db_table = 'prob_cate'
+
+    def __str__(self):
+        return self.pc_value
+
+class ProbType(models.Model):
+    pt_id = models.AutoField(primary_key=True)
+    pt_value = models.CharField(max_length=200)
+    class Meta:
+        managed = False
+        db_table = 'prob_type'
+
+class ProbDiff(models.Model):
+    pd_id = models.AutoField(primary_key=True)
+    pd_value = models.CharField(max_length=200)
+    class Meta:
+        managed = False
+        db_table = 'prob_diff'
+
+class Problem(models.Model):
     p_id = models.AutoField(primary_key=True)
-    p_category = models.CharField(max_length=200)
     p_question = models.TextField()
-    p_type = models.IntegerField()
-    p_diff = models.IntegerField()
     p_code = models.TextField(null=True)
+    pc_id = models.ForeignKey(ProbCate, db_column='pc_id', on_delete=models.CASCADE)
+    pt_id = models.ForeignKey(ProbType, db_column='pt_id', on_delete=models.CASCADE)
+    pd_id = models.ForeignKey(ProbDiff, db_column='pd_id', on_delete=models.CASCADE)
     class Meta:
         managed = False
         db_table = 'problems'
 
-class AnswerBasic(models.Model):
+class Answer(models.Model):
     a_id = models.AutoField(primary_key=True)
-    p_id = models.ForeignKey(ProblemBasic, db_column='p_id', on_delete=models.CASCADE)
+    p_id = models.ForeignKey(Problem, db_column='p_id', on_delete=models.CASCADE)
     a_value = models.TextField()
     a_correct = models.BooleanField()
 
