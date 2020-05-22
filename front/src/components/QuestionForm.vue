a0c74c255f78fa029488ef46cf013dc154110278
EHwooKim <ehwoo0707@naver.com>
2020-05-22 10:47:23 +0900

    Updating adminPage

--- front/src/components/QuestionForm.vue
+++ front/src/components/QuestionForm.vue
@@ -39,10 +39,10 @@
         <div v-if="questionId===2">
           <input type="text" placeholder="정답 입력"  class="input-default" v-model="answer">
           <h2>보기</h2>
-          <input type="text" name="example" class="input-default" v-model="example[0]">
-          <input type="text" name="example" class="input-default" v-model="example[1]">
-          <input type="text" name="example" class="input-default" v-model="example[2]">
-          <input type="text" name="example" class="input-default" v-model="example[3]">
+          <input type="text" name="examples" class="input-default" v-model="examples[0]">
+          <input type="text" name="examples" class="input-default" v-model="examples[1]">
+          <input type="text" name="examples" class="input-default" v-model="examples[2]">
+          <input type="text" name="examples" class="input-default" v-model="examples[3]">
         </div>
         <div v-if="questionId===3">
           <input type="radio" name="answer" value="O" v-model="answer">O
@@ -63,7 +63,7 @@
         <div v-if="questionId===2">
           <h2>보기</h2>
           <ol>
-            <li v-for="(e, i) in example" :key="i">{{e}}</li>
+            <li v-for="(e, i) in examples" :key="i">{{e}}</li>
           </ol>
         </div>
       </div>
@@ -80,15 +80,16 @@ import axios from 'axios'
         questionId:1,
         category: '',
         difficulty: 1,
-        showCodeBox: false,
         questionText: '',
+        showCodeBox: false,
         codeText:'',
         answer: '',
-        example: ['', '', '', ''],
+        examples: ['', '', '', ''],
       }
     },
     methods: {
       onClickSubmit() {
+        if (!this.category || !this.questionText || !this.answer) return alert('잘못된 입력입니다.(카테고리 미선택. 문제, 답변 미입력 등..)')
         let question = {
           problems: {
             p_question: this.questionText,
@@ -96,18 +97,19 @@ import axios from 'axios'
             pt_id: this.questionId,
             pd_id: this.difficulty,
           },
-          // 정답 백엔드 통일시켜주세요!!!!!!!!!!!!!
+          answer: this.answer
         }
-        if (this.codeText) question['p_code'] = this.codeText.replace(/"/gi, '\\"')
-        console.log(question)
-        // axios.post('http://k02b1011.p.ssafy.io:8085/', {hi:1})
+        if (this.showCodeBox) question['problems']['p_code'] = this.codeText.replace(/"/gi, '\\"')
+        if (this.questionId == 2) question['examples'] = this.example
+        // axios.post('http://k02b1011.p.ssafy.io:8085/api/problems/create_prob/', question)
         //   .then(res => console.log(res))
+        axios.get('https://baconipsum.com/api/?type=meat-and-filler')
+          .then(res => console.log(res))
       },
       changeQuestionType() {
         this.questionId = (this.questionId + 1) % 3 + 1
       },
       setInputDefault() {
-        console.log(this.codeText)
         if (event.keyCode === 9) {
           event.preventDefault()
           this.codeText += '  ' 
