72d623437fa23e7e872f0dd61595d7662df04d10
yg1110 <younggil9488@gmail.com>
2020-05-23 17:32:06 +0900

    feature : Category API + vuex 연동

--- front/src/main.js
+++ front/src/main.js
@@ -4,6 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import VueSession from "vue-session";
+import axios from "axios";
 
 Vue.config.productionTip = false;
 Vue.use(VueSession);
@@ -11,5 +12,5 @@ Vue.use(VueSession);
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount("#app");
