5be8e1826f806d3e5610e39ed1dacf7f24adcc24
yg1110 <younggil9488@gmail.com>
2020-05-26 16:25:21 +0900

    feature : QuestionDeatil OX 퀴즈, 단답형

--- front/src/components/Question/Detail/Problem.vue
+++ front/src/components/Question/Detail/Problem.vue
@@ -1,45 +1,42 @@
 <template>
-  <body>
-    <div id="subhead"></div>
-    <ul id="questionsList">
-      <section
-        :class="i === 0 ? 'current' : ''"
-        v-for="(q, i) in questionList"
-        :key="i"
-      >
-        <p class="problem_question">{{ q.p_question }}</p>
+<body>
+  <div id="subhead"></div>
+  <ul id="questionsList">
+    <section :class="i === 0 ? 'current' : ''" v-for="(q, i) in questionList" :key="i">
+      <p class="problem_question">{{ q.p_question }}</p>
 
-        <div v-highlight v-if="q.p_code !== null">
-          <pre class="language-javascript">
-              <code>
-                {{q.p_code}}
-              </code>
-            </pre>
-        </div>
+      <div v-highlight v-if="q.p_code !== null">
+        <pre class="language-javascript">
+          <code>
+            {{q.p_code}}
+          </code>
+        </pre>
+      </div>
+      <div v-if="q.pt_id !== 4">
         <span class="questionItem" v-for="a in q.answers" :key="a.a_id">
-          <input
-            :id="a.a_value"
-            type="radio"
-            :name="q.answers"
-            :value="a.a_value"
-          />
-          <label :for="a.a_value">{{ a.a_value }}</label>
+          <input :id="a.a_id" type="radio" :name="q.p_id" :value="a.a_value" />
+          <label :for="a.a_id">{{ a.a_value }}</label>
         </span>
-        <a
-          class="nextButton"
-          @click="
+      </div>
+      <div v-else>
+        <br />
+        <input class="questionItem" type="text" v-model="shortAnswer" />
+      </div>
+      <a
+        class="nextButton"
+        @click="
             nextQuestion(
               i,
               questionList.length - 1,
               q.currentAnswer,
-              q.currentIndex
+              q.currentIndex,
+              q.pt_id === 4
             )
           "
-          >정답 확인</a
-        >
-      </section>
-    </ul>
-  </body>
+      >정답 확인</a>
+    </section>
+  </ul>
+</body>
 </template>
 
 <script>
@@ -53,7 +50,8 @@ export default {
   data() {
     return {
       questions: document.getElementsByTagName("section"),
-      index: 0
+      index: 0,
+      shortAnswer: ""
     };
   },
 
@@ -78,29 +76,45 @@ export default {
   },
 
   methods: {
-    nextQuestion(i, size, currentAnswer, currentIndex) {
+    nextQuestion(i, size, currentAnswer, currentIndex, flag) {
       let answer = this.questions[i].getElementsByTagName("input");
+      // let button = document.getElementsByClassName("nextButton");
       let problems = document.getElementsByClassName("questionItem");
-      let button = document.getElementsByClassName("nextButton");
-      answer.forEach((v, i) => {
-        if (v.checked) {
-          if (v.value === currentAnswer) {
-            problems[this.index + i].setAttribute(
-              "style",
-              "background:#99f19e"
-            );
-          } else {
-            problems[this.index + i].setAttribute(
-              "style",
-              "background:#ff4e50"
-            );
-            problems[currentIndex].setAttribute("style", "background:#99f19e");
-          }
-        }
-      });
-      this.index += answer.length;
 
+      if (!flag) {
+        answer.forEach((v, i) => {
+          if (v.checked) {
+            if (v.value === currentAnswer[0]) {
+              problems[this.index + i].setAttribute(
+                "style",
+                "background:#99f19e"
+              );
+            } else {
+              problems[this.index + i].setAttribute(
+                "style",
+                "background:#ff4e50"
+              );
+              problems[currentIndex].setAttribute(
+                "style",
+                "background:#99f19e"
+              );
+            }
+          }
+        });
+        this.index += answer.length;
+      } else {
+        const res = currentAnswer.some(v => {
+          return v.toLowerCase() === this.shortAnswer.toLowerCase();
+        });
+
+        if (res) {
+          alert("맞음");
+        } else {
+          alert("틀림");
+        }
+      }
       setTimeout(() => {
+        this.shortAnswer = "";
         if (i !== size) {
           this.questions[i].className = "";
           this.questions[i + 1].className = "current";
