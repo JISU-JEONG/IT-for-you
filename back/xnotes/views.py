28c2017bee3329807bd40abe66173199593a5252
김기은 <w.snowhack@gmail.com>
2020-05-28 14:50:15 +0900

    mod: Incorrect Note

--- back/xnotes/views.py
+++ back/xnotes/views.py
@@ -9,56 +9,33 @@ from problems.models import *
 from problems.serializers import *
 from .serializers import *
 from drf_yasg.utils import swagger_auto_schema
-# Create your views here.
-# class Note(APIView):
-#     @swagger_auto_schema(request_body=MyNoteSerializer)
-#     def post(self, request):
-#         serializer = MyNoteSerializer(data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             serializer.save()
-#         return JsonResponse({'message': 'Success'})
 
 class MyNote(APIView):
     def get(self, request, user_id):
         mynotes = Xnote.objects.filter(user_id=user_id)
-        # embed()
         serializer = MyNoteListSerializer(mynotes, many=True)
         return Response(serializer.data)
     
     def post(self, request, user_id):
-        serializer = XnoteSerializer(data=request.data)
-        embed()
+        data = request.data
+        data['user'] = user_id
+        mynote = Xnote.objects.filter(user=user_id, prob=data.get('prob'))
+        if mynote:
+            mynote = get_object_or_404(Xnote, id=mynote[0].id)
+            serializer = XnoteSerializer(data=data, instance=mynote)
+        else:
+            serializer = XnoteSerializer(data=data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
         return JsonResponse({'message': 'Success'})
 
 class MyNoteDetail(APIView):
-    def get(self, request, note_id):
+    def get(self, request, user_id, note_id):
         mynote = get_object_or_404(Xnote, pk=note_id)
         serializer = MyNoteDetailSerializer(mynote)
         return Response(serializer.data)
     
-    def delete(self, request, note_id):
+    def delete(self, request, user_id, note_id):
         mynote = get_object_or_404(Xnote, pk=note_id)
         mynote.delete()
         return JsonResponse({'message': 'Success'})
-
-# class MyNote2(APIView):
-#     def get(self, request, user_id, prob_id):
-#         pass
-        
-#     def post(self, request, user_id, prob_id):
-#         # embed()
-#         mynotes = Xnote.objects.filter(user_id=user_id, prob_id=prob_id)
-#         if mynotes:
-#             mynotes.delete()
-
-#         serializer = MyNoteSerializer(data={'u_answer': request.data.get('u_answer'), 'user': user_id, 'prob': prob_id})
-#         if serializer.is_valid(raise_exception=True):
-#             serializer.save()
-#         return JsonResponse({'message': 'Success'})
-
-#     def delete(self, request, user_id, prob_id):
-#         embed()
-#         mynote = get_object_or_404(Xnote, user_id=user_id)
-        
