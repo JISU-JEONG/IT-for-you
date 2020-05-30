b215529eb90d2444bc66b0bd97edc954014f1f5a
김기은 <w.snowhack@gmail.com>
2020-05-30 15:31:46 +0900

    feat: My Favorite Problem:

--- back/myprobs/models.py
+++ back/myprobs/models.py
@@ -2,3 +2,7 @@ from django.db import models
 from accounts.models import User
 from problems.models import Problem
 # Create your models here.
+
+class MyProb(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  prob = models.ForeignKey(Problem, on_delete=models.CASCADE)
