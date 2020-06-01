48dffe86e4bf40a8a3c2c31d5b05d95dda033eaf
김기은 <w.snowhack@gmail.com>
2020-06-01 11:40:41 +0900

    feat: My Favorite Problems

--- back/myprobs/serializers.py
+++ back/myprobs/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import *
+from problems.serializers import *
 
 class MyProbSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyProb
         fields = ('id', 'user', 'prob',)
+
+class MyProbListSerializer(MyProbSerializer):
+    problems = ProblemSerializer(source='prob')
+    class Meta(MyProbSerializer.Meta):
+        fields = MyProbSerializer.Meta.fields + ('problems',)
+
+class MyProbDetailSerializer(MyProbSerializer):
+    problems = ProblemSerializer(source='prob')
+    answers = AnswerSerializer(source='prob.answer_set', many=True)
+    class Meta(MyProbSerializer.Meta):
+        fields = MyProbSerializer.Meta.fields + ('problems', 'answers',)
