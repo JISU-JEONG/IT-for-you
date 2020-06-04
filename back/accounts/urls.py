72ab8cb79d6fe0456297adba548d6b4318bfb23f
김기은 <w.snowhack@gmail.com>
2020-06-04 14:37:59 +0900

    add: My Interview Get List and Detail

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -6,9 +6,5 @@ urlpatterns = [
     path('signup/',views.user_signup),
     path('user/',views.user),
     path('user_delete/', views.user_delete),
-    path('voice/', views.voice),
     path('users/', views.users),
-    path('get_interview/<int:p_id>/', views.get_interview),
-    path('get_audio/<int:p_id>/', views.get_audio),
-    # path('add_problem/<int:problem_pk>/', views.add_problem),
 ]
