9ba209252b50f7f22efc9b4501e0921def621d0b
EHwooKim <ehwoo0707@naver.com>
2020-06-08 17:12:57 +0900

    Fix login bug

--- front/src/api/api.service.js
+++ front/src/api/api.service.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 import { API_URL } from "@/api/config";
 
-const token = JSON.parse(sessionStorage.getItem("vue-session-key"))["jwt"];
-console.log(token);
 export default axios.create({
   baseURL: API_URL,
   headers: {
-    Authorization: `JWT ${token}`,
     "Content-type": "application/json"
   }
 });
