39445e0746fd92b0f5ff2812a6dc1ac1acd39a25
EHwooKim <ehwoo0707@naver.com>
2020-06-08 09:05:05 +0900

    Update Interview

--- front/src/api/api.service.js
+++ front/src/api/api.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { API_URL } from "@/api/config";
 
+
 export default axios.create({
   baseURL: API_URL,
   headers: {
---
+++ front/src/views/InterviewMyNote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main-container">
+    <p>이곳은 인터뷰 풀어본 문젱비니다.</p>
+  </div>
+</template>
+<script>
+  import axios from '@/api/api.service.js'
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style scoped>
+
+</style>
